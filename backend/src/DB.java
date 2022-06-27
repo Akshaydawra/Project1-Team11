01f12514babf1b6cdb37dc688b2ed2ea95d100d6
Akshay Dawra <83584941+Akshaydawra@users.noreply.github.com>
2022-06-27 13:46:19 +0530

    Update DB.java

--- backend/src/DB.java
+++ backend/src/DB.java
@@ -3,15 +3,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author MY HP
- */
+
 public class DB {
           public static void main(String[] args)
     {
